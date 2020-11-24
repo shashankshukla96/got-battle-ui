6f4da3d41dc1d200752de6c85ddc75904fb940ba
Shashank Shukla <shashankshukla96@gmail.com>
2020-11-24 19:32:45 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,80 +1,149 @@
 import "./App.css";
-import React, { useState } from "react";
-import { Route, Link, BrowserRouter, Switch } from "react-router-dom";
+import React, { useEffect, useState } from "react";
+import { Route, BrowserRouter, Switch } from "react-router-dom";
 import Autocomplete from "react-autocomplete";
 import Battle from "./components/Battle";
+import Axios from "axios";
+import url from "./config/api";
 
 function App() {
-  const [search1, setSearch1] = useState("");
-  const [search2, setSearch2] = useState("");
-  const [search3, setSearch3] = useState("");
+    const [location, setLocation] = useState("");
+    const [king, setKing] = useState("");
+    const [type, setType] = useState("");
+    const [places, setPlaces] = useState();
+    const [filteredPlaces, setFilteredPlaces] = useState([]);
+    const [errMessage, setErrMessage] = useState();
+    const [battles, setBattles] = useState([]);
+    const [count, setCount] = useState();
 
+    useEffect(() => {
+        Axios.get(url + "/list")
+            .then((data) => {
+                setPlaces(data.data.data);
+                setFilteredPlaces(data.data.data);
+            })
+            .catch((err) => {
+                setErrMessage(err.message);
+            });
 
-  const search=()=>{
-      console.log(search1,search2,search3)
-  }
-  return (
-    <BrowserRouter>
-      <div className="nav">
-        <div className=" search search1">
-          <Autocomplete
-            getItemValue={(item) => item.label}
-            items={[{ label: "apple" }, { label: "banana" }, { label: "pear" }]}
-            renderItem={(item, isHighlighted) => (
-              <div
-                style={{ background: isHighlighted ? "lightgray" : "white" }}
-              >
-                {item.label}
-              </div>
+        Axios.get(url + "/count")
+            .then((data) => {
+                setCount(data.data.data);
+            })
+            .catch((err) => {
+                setErrMessage(err.message);
+            });
+    }, []);
+
+    const search = () => {
+        const searchBody = {
+            king: king ? king : "",
+            location: location ? location : "",
+            type: type ? type : "",
+        };
+
+        let str = [];
+        for (var p in searchBody)
+            if (searchBody.hasOwnProperty(p)) {
+                str.push(
+                    encodeURIComponent(p) +
+                        "=" +
+                        encodeURIComponent(searchBody[p])
+                );
+            }
+
+        const searchUrl = url + "/search?" + str.join("&");
+        Axios.get(searchUrl)
+            .then((data) => {
+                setBattles(data.data.data);
+            })
+            .catch((err) => {
+                setErrMessage(err.message);
+            });
+    };
+    return (
+        <>
+            <div className='nav'>
+                <div className=' search search1'>
+                    <Autocomplete
+                        getItemValue={(item) => item}
+                        items={filteredPlaces}
+                        renderItem={(item, isHighlighted) => (
+                            <div
+                                key={item}
+                                style={{
+                                    background: isHighlighted
+                                        ? "lightgray"
+                                        : "white",
+                                }}>
+                                {item}
+                            </div>
+                        )}
+                        value={location}
+                        onChange={(e) => {
+                            setLocation(e.target.value);
+                            const filteredPlaces = places.filter((place) => {
+                                if (place.startsWith(e.target.value)) {
+                                    return true;
+                                } else return false;
+                            });
+
+                            setFilteredPlaces(filteredPlaces);
+                        }}
+                        onSelect={(val) => setLocation(val)}
+                        renderInput={(props) => {
+                            return (
+                                <input
+                                    {...props}
+                                    placeholder='Search Location'
+                                />
+                            );
+                        }}
+                    />
+                </div>
+                <div className='search search2'>
+                    <input
+                        placeholder='Search King'
+                        onChange={(e) => setKing(e.target.value)}
+                        value={king}
+                    />
+                </div>
+                <div className='search search3'>
+                    <input
+                        placeholder='Search Battle type'
+                        onChange={(e) => setType(e.target.value)}
+                        value={type}
+                    />
+                </div>
+
+                <button className='button0 button1' onClick={() => search()}>
+                    Search
+                </button>
+            </div>
+            {battles.length === 0 ? (
+                <p
+                    style={{
+                        color: "black",
+                        fontWeight: "700",
+                        marginLeft: 10,
+                    }}>
+                    Please Select any Search criteria, out of {count} battles.
+                </p>
+            ) : (
+                ""
             )}
-           
-            value={search1}
-            onChange={(e) => setSearch1(e.target.value)}
-            onSelect={(val) => setSearch1(val)}
-          />
-        </div>
-        <div className="search search2">
-          <Autocomplete
-            getItemValue={(item) => item.label}
-            items={[{ label: "apple" }, { label: "banana" }, { label: "pear" }]}
-            renderItem={(item, isHighlighted) => (
-              <div
-                style={{ background: isHighlighted ? "lightgray" : "white" }}
-              >
-                {item.label}
-              </div>
-            )}
-            value={search2}
-            onChange={(e) => setSearch2(e.target.value)}
-            onSelect={(val) => setSearch2(val)}
-          />
-        </div>
-        <div className="search search3">
-          <Autocomplete
-            getItemValue={(item) => item.label}
-            items={[{ label: "apple" }, { label: "banana" }, { label: "pear" }]}
-            renderItem={(item, isHighlighted) => (
-              <div
-                style={{ background: isHighlighted ? "lightgray" : "white" }}
-              >
-                {item.label}
-              </div>
-            )}
-            value={search3}
-            onChange={(e) => setSearch3(e.target.value)}
-            onSelect={(val) => setSearch3(val)}
-          />
-        </div>
 
-        <button className="button0 button1" onClick={()=>search()}>Search</button>
-      </div>
-
-      <Switch>
-        <Route exact path="/" component={Battle} />
-        {/* <Route path="/battle" component={Battle} /> */}
-      </Switch>
-    </BrowserRouter>
-  );
+            {errMessage ? (
+                <p style={{ color: "red", fontWeight: "700", marginLeft: 10 }}>
+                    {errMessage}
+                </p>
+            ) : (
+                battles.map((battle, index) => {
+                    return <Battle key={index} battle={battle} />;
+                })
+            )}
+        </>
+    );
 }
 
 export default App;
---
+++ src/config/api.js
@@ -0,0 +1,3 @@
+const url = "http://localhost:5000";
+
+module.exports = url;
